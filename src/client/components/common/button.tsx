9caac819fe3086420b1421c18cf7c1573b3d7d7e
Artemis Davakou <artemis.davakou@chasacademy.se>
2025-06-01 06:25:33 +0200

    chore: add border to secondary button

--- src/client/components/common/button.tsx
+++ src/client/components/common/button.tsx
@@ -7,11 +7,12 @@ type ButtonProps = {
 };
 
 const baseClasses =
-  "px-4 py-4 font-bold rounded-2xl transition-colors duration-300 leading-[12.8px] text-[14px]";
+  "px-4 py-4 font-bold rounded-2xl transition-colors duration-300 leading-[12.8px] text-[14px] border-2 border-transparent cursor-pointer";
 
 const variantClasses = {
   primary: "bg-black text-white hover:bg-white hover:text-black",
-  secondary: "bg-white text-black hover:bg-black hover:text-white",
+  secondary:
+    "bg-white text-black hover:bg-black hover:text-white hover:border-2 hover:border-white",
   transparent:
     "bg-transparent border-2 border-black text-black hover:bg-black hover:text-white",
   danger: "bg-[#EB634B] text-white hover:bg-[#C14A32]",
