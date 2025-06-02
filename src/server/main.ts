610b6c662b5987e097d0dedacb1f315d175636ff
Artemis Davakou <artemis.davakou@chasacademy.se>
2025-06-02 10:08:04 +0200

    chore: enable commented out code, database

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,12 +1,12 @@
 import express from "express";
 import ViteExpress from "vite-express";
-// import mongoose from "mongoose";
-// import { load } from "ts-dotenv";
+import mongoose from "mongoose";
+import { load } from "ts-dotenv";
 
-// const env = load({
-//   DB_USER: String,
-//   DB_PASSWORD: String,
-// })
+const env = load({
+  DB_USER: String,
+  DB_PASSWORD: String,
+});
 
 const app = express();
 
@@ -14,7 +14,9 @@ app.get("/api/test", (_, res) => {
   res.send("this is an api response!!!!");
 });
 
-// await mongoose.connect(`mongodb+srv://${env.DB_USER}:${env.DB_PASSWORD}@cluster0.jfdd8.mongodb.net/u09`);
+await mongoose.connect(
+  `mongodb+srv://${env.DB_USER}:${env.DB_PASSWORD}@cluster0.jfdd8.mongodb.net/u09`
+);
 
 ViteExpress.listen(app, 3000, () =>
   console.log("Server is listening on port 3000...")
