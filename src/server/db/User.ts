0c7e53ef85e03b2fa617e7577831b107f0c792ab
Terracrafty <63670539+Terracrafty@users.noreply.github.com>
2025-06-03 15:54:03 +0200

    feat: http error handling, user controller, auth middleware

--- src/server/db/User.ts
+++ src/server/db/User.ts
@@ -1,19 +1,34 @@
 import { model, Schema, Types } from "mongoose";
+import { createHash, randomBytes } from "crypto";
 
 export interface IUser {
     name:string;
     email:string;
     password:string;
+    salt?:string;
     lists:Array<Types.ObjectId>;
 }
 
 export const userSchema = new Schema<IUser>({
     name: { type: String, required: true },
-    email: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    salt: { type: String },
     lists: [{type: Schema.Types.ObjectId, ref: "List"}]
 }, {
     timestamps: true
+});
+
+userSchema.pre("save", function(next) { //password hashing
+    if (this.isModified("password") || this.isNew) {
+        const salt = randomBytes(128).toString("base64");
+        const hashedPassword = createHash("sha256")
+            .update(this.password + salt)
+            .digest("hex");
+        this.salt = salt;
+        this.password = hashedPassword;
+    }
+    next();
 });
 
 export const User = model("User", userSchema);
---
+++ src/server/middleware/auth.ts
@@ -0,0 +1,29 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import { load } from "ts-dotenv";
+import { handleHttpError, HttpError } from "../HttpError.ts";
+import { User } from "../db/User.ts";
+
+const env = load({
+    JWT_SECRET:String,
+});
+
+export const authorize = async (req:Request, res:Response, next:NextFunction) => {
+    try {
+        await User.findById(req.params.id).orFail(new HttpError(404, "User not found"));
+        const token = req.header("Authorization")?.replace("Bearer ", "");
+        if (!token) {
+            res.status(401).end();
+            return;
+        }
+        const decoded = jwt.verify(token, env.JWT_SECRET);
+        if (decoded.sub == req.params.id) {
+            next();
+        } else {
+            res.status(403).end();
+            return;
+        }
+    } catch (e) {
+        handleHttpError(e, res);
+    }
+};
