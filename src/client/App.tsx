703668915320ea7927df3f93815a966bbdd1c3c4
Artemis <Artemis.davakou@chasacademy.se>
2025-06-07 06:14:02 +0200

    feat: add recipe details page

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/home";
 import Recommendation from "./pages/Recommendation";
-import DetailRecipe from "./components/common/details-recipe";
+import DetailRecipe from "./pages/details-recipe";
 import Recipes from "./pages/recipes";
 import Trending from "./pages/trending";
 import Header from "./components/common/header";
