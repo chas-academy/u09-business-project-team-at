d0a8ab68bfda3583261a0806eb21bb2ad5e3f1ec
Artemis <Artemis.davakou@chasacademy.se>
2025-06-07 06:08:18 +0200

    chore: fix variant: transparent on button componen

--- src/client/components/common/button.tsx
+++ src/client/components/common/button.tsx
@@ -10,14 +10,14 @@ type ButtonProps = {
 };
 
 const baseClasses =
-  "px-6 py-2 font-bold rounded transition-colors duration-500 leading-[12.8px] text-[14px] border-2 border-transparent cursor-pointer";
+  "px-6 py-2 font-bold rounded transition-colors duration-500 leading-[12.8px] text-[14px] border-2  cursor-pointer";
 
 const variantClasses = {
   primary: "bg-black text-white hover:bg-white hover:text-black",
   secondary:
     "bg-white text-black hover:bg-transparent hover:border-white hover:text-white",
   transparent:
-    "bg-transparent border-2 border-black text-black hover:bg-black hover:text-white",
+    "bg-transparent border-solid border-black text-black hover:bg-black hover:text-white",
   danger: "bg-[#EB634B] text-white hover:bg-[#C14A32]",
 };
 
