226b140277bea9cacf1c21ae930638793e1506d3
Artemis <Artemis.davakou@chasacademy.se>
2025-06-07 06:03:13 +0200

    feat: att recipe detail route

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/home";
-import Recommendation from "./pages/recommendation";
+import Recommendation from "./pages/Recommendation";
+import DetailRecipe from "./components/common/details-recipe";
 import Recipes from "./pages/recipes";
 import Trending from "./pages/trending";
 import Header from "./components/common/header";
 import Profile from "./pages/profile";
+import Categories from "./pages/categories";
 
 function App() {
   return (
@@ -16,7 +18,9 @@ function App() {
         <Route path="/recipes" element={<Recipes />} />
         <Route path="/trending" element={<Trending />} />
         <Route path="/recommendation" element={<Recommendation />} />
+        <Route path="/recipe/:id" element={<DetailRecipe />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/categories" element={<Categories />} />
       </Routes>
     </Router>
   );
