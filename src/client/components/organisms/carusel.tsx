c179d91c2fcc51359c1c108f178dc69ee91fe443
Artemis <Artemis.davakou@chasacademy.se>
2025-06-09 04:38:44 +0200

    feat: render different data depending on renderType

--- src/client/components/organisms/carusel.tsx
+++ src/client/components/organisms/carusel.tsx
@@ -4,17 +4,32 @@ import { RecipeService } from "../../services/recipe.service";
 import { Recipes, Recipe } from "../../models/recipe.model";
 import SectionGrid from "../molecules/section-grid";
 
-export default function Carusel() {
+type CaruselProps = {
+  RenderType: "trending" | "recipes" | "userList";
+};
+
+export default function Carusel(props: CaruselProps) {
   const [recipes, setRecipes] = useState<Recipe[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<null | Error>(null);
 
   useEffect(() => {
     try {
-      RecipeService.getRecipes().then((data: Recipes) => {
-        setRecipes(data.recipes);
-        setLoading(false);
-      });
+      if (props.RenderType === "recipes") {
+        RecipeService.getRecipes().then((data: Recipes) => {
+          setRecipes(data.recipes);
+          setLoading(false);
+        });
+      }
+
+      if (props.RenderType === "trending") {
+        RecipeService.getTrendingRecipes().then((data: Recipes) => {
+          setRecipes(data.recipes);
+          setLoading(false);
+        });
+      }
+
+      // TODO: If RenderType is userList, implement logic to fetch user-specific recipes
     } catch (error) {
       console.error("Error fetching recipes from service:", error);
       setError(error as Error);
@@ -22,7 +37,17 @@ export default function Carusel() {
     }
   }, []);
 
-  if (loading) return <div>Loading...</div>;
+  if (loading) {
+    return (
+      <div className="overflow-hidden">
+        <SectionGrid>
+          {Array.from({ length: 12 }).map((_, index) => (
+            <RecipeCard key={index} isLoading={true} />
+          ))}
+        </SectionGrid>
+      </div>
+    );
+  }
   if (error) return <div>Error: {error.message}</div>;
 
   return (
@@ -30,13 +55,15 @@ export default function Carusel() {
       <SectionGrid>
         {recipes.map((recipe: Recipe, index: number) => (
           <RecipeCard
-            id={recipe.id}
-            key={index}
-            title={recipe.name}
-            image={recipe.image}
-            category={recipe.cuisine}
-            cookTimeMinutes={recipe.cookTimeMinutes}
-            rating={recipe.rating}
+            key={recipe.id}
+            recipe={{
+              id: recipe.id,
+              title: recipe.name,
+              image: recipe.image,
+              category: recipe.cuisine,
+              cookTimeMinutes: recipe.cookTimeMinutes,
+              rating: recipe.rating,
+            }}
           />
         ))}
       </SectionGrid>
