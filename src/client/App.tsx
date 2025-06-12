05f01b05cd971812485f45e89a5659386efabd28
Artemis <Artemis.davakou@chasacademy.se>
2025-06-12 14:40:05 +0200

    feat: add user provider to app.tsx

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import DetailRecipe from "./pages/details-recipe";
 import Recipes from "./pages/recipes";
@@ -6,21 +6,24 @@ import Header from "./components/molecules/header";
 import Profile from "./pages/profile";
 import Trending from "./pages/trending";
 import { ModalProvider } from "./context/ModalContext";
+import { UserProvider } from "./contexts/UserContext";
 
 function App() {
   return (
-    <ModalProvider>
-      <Router>
-        <Header variants="guest" />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/recipes" element={<Recipes />} />
-          <Route path="/recipe/:id" element={<DetailRecipe />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/trending" element={<Trending />} />
-        </Routes>
-      </Router>
-    </ModalProvider>
+    <UserProvider>
+      <ModalProvider>
+        <Router>
+          <Header variants="guest" />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/recipes" element={<Recipes />} />
+            <Route path="/recipe/:id" element={<DetailRecipe />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/trending" element={<Trending />} />
+          </Routes>
+        </Router>
+      </ModalProvider>
+    </UserProvider>
   );
 }
 
