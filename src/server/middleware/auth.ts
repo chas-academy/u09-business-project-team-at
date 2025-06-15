6f2342c8b700c9939170dab5ab24ef960db9eff3
Artemis <Artemis.davakou@chasacademy.se>
2025-06-15 21:58:06 +0200

    feat: enhance list management with validation and soft delete functionality

--- src/server/middleware/auth.ts
+++ src/server/middleware/auth.ts
@@ -1,29 +1,32 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 import { load } from "ts-dotenv";
-import { handleHttpError, HttpError } from "../HttpError.ts";
-import { UserModel } from "../db/User.ts";
+import { handleHttpError } from "../HttpError.ts";
 
 const env = load({
-    JWT_SECRET:String,
+  JWT_SECRET: String,
 });
 
-export const authorize = async (req:Request, res:Response, next:NextFunction) => {
-    try {
-        await UserModel.findById(req.params.id).orFail(new HttpError(404, "User not found"));
-        const token = req.header("Authorization")?.replace("Bearer ", "");
-        if (!token) {
-            res.status(401).end();
-            return;
-        }
-        const decoded = jwt.verify(token, env.JWT_SECRET);
-        if (decoded.sub == req.params.id) {
-            next();
-        } else {
-            res.status(403).end();
-            return;
-        }
-    } catch (e) {
-        handleHttpError(e, res);
+export const authorize = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const token = req.header("Authorization")?.replace("Bearer ", "");
+    if (!token) {
+      res.status(401).end();
+      return;
     }
+    const decoded = jwt.verify(token, env.JWT_SECRET);
+    if (decoded.sub) {
+      req.params.userId = decoded.sub.toString();
+      next();
+    } else {
+      res.status(403).end();
+      return;
+    }
+  } catch (e) {
+    handleHttpError(e, res);
+  }
 };
