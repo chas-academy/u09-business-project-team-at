44f28aeee07e8a7ad2b2b59fbb3d1e027c0aa251
Artemis <Artemis.davakou@chasacademy.se>
2025-06-15 21:59:39 +0200

    feat: update list management to include recipe addition and enhance update functionality

--- src/client/services/list.service.ts
+++ src/client/services/list.service.ts
@@ -1,6 +1,5 @@
 import { ListRepository } from "../repositories/list.repository";
 import { List } from "../models/list.model";
-import { get } from "http";
 export const ListService = {
   async getAllLists(token: string, id: string): Promise<List[]> {
     return await ListRepository.getAllList(token, id);
@@ -9,10 +8,9 @@ export const ListService = {
     token: string,
     id: string,
     listId: string,
-    recipeId: string,
     list: List
   ): Promise<List> {
-    return await ListRepository.updateList(token, id, listId, recipeId, list);
+    return await ListRepository.updateList(token, id, listId, list);
   },
   async createList(token: string, id: string, name: string): Promise<List> {
     return await ListRepository.createList(token, id, name);
@@ -20,5 +18,22 @@ export const ListService = {
 
   async getListById(token: string, listId: string): Promise<List> {
     return await ListRepository.getListById(token, listId);
+  },
+
+  async deleteList(
+    token: string,
+    userId: string,
+    listId: string
+  ): Promise<void> {
+    return ListRepository.deleteList(token, userId, listId);
+  },
+
+  async updateListName(
+    token: string,
+    userId: string,
+    listId: string,
+    name: string
+  ): Promise<List> {
+    return await ListRepository.updateListName(token, userId, listId, name);
   },
 };
