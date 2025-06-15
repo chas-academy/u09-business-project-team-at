9aa80ec52784b006ad019e5a489d9150613c6566
Artemis <Artemis.davakou@chasacademy.se>
2025-06-15 22:00:10 +0200

    feat: enhance RecipeCards component to support recipe deletion functionality

--- src/client/components/organisms/recipe-cards.tsx
+++ src/client/components/organisms/recipe-cards.tsx
@@ -1,28 +1,43 @@
 import RecipeCard from "../molecules/recipe-card";
 import { Recipe } from "../../models/recipe.model";
+import { IonContent, IonIcon } from "@ionic/react";
+import { trashOutline } from "ionicons/icons";
 
 type RecipeProps = {
   recipes: Recipe[];
+  handler?: (recipeId: string) => Promise<void>;
 };
 
-export default function RecipeCards({ recipes }: RecipeProps) {
+export default function RecipeCards({ recipes, handler }: RecipeProps) {
   return (
-    <div
-      className="grid justify-items-center
-                    grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 pt-4 gap-y-8 gap-x-4"
-    >
+    <div className="grid justify-items-center grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 pt-4 gap-y-8 gap-x-4">
       {recipes.map((recipe: Recipe, index: number) => (
-        <RecipeCard
+        <div
           key={index}
-          recipe={{
-            id: recipe.id,
-            title: recipe.name,
-            image: recipe.image,
-            category: recipe.cuisine,
-            cookTimeMinutes: recipe.cookTimeMinutes,
-            rating: recipe.rating,
-          }}
-        />
+          className="flex items-center justify-center w-full relative"
+        >
+          <RecipeCard
+            key={index}
+            recipe={{
+              id: recipe.id,
+              title: recipe.name,
+              image: recipe.image,
+              category: recipe.cuisine,
+              cookTimeMinutes: recipe.cookTimeMinutes,
+              rating: recipe.rating,
+            }}
+          />
+          {handler && (
+            <div className="absolute top-2 right-2 bg-[#000000B9] hover:bg-white cursor-pointer rounded-full shadow-md justify-center items-center flex p-2">
+              <button
+                className="text-red-500 hover:text-red-700 text-2xl justify-center items-center flex cursor-pointer"
+                onClick={() => handler(recipe.id.toString())}
+              >
+                <IonIcon icon={trashOutline} />
+              </button>
+            </div>
+          )}
+        </div>
       ))}
     </div>
   );
